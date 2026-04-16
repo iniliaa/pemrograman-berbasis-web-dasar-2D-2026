cac7942476e4e925c97dff88953a29d3c6e80a0a
iniliaa <plinaaaww@gmail.com>
2026-04-16 11:42:20 +0700

    250441100007_MAULINAAPRILLIAWATI_RACHMADAZIZRAZARIKHA_MODUL3

---
+++ modul-3/250441100007_MaulinaAprilliawati/Tailwindcss/tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    container: {
+      center: true,
+      padding: '16px'
+    },
+    screens: {
+      '2xl': '1320px',
+    }
+  },
+  
+  plugins: [],
+}
+
